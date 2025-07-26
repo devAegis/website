3de3b7f9199ba81c196aaf4aacaff09f31dcfc8b
Rufaro Mandipa <mandipaobey@gmail.com>
2025-07-26 16:44:16 +0200

    commit after tailwind v4

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './landing.css'; // Assuming you have some styles in Landing.css
 
 function Landing() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-100">
-      <p className="x1 text-4xl font-bold text-blue-700">Welcome to the Landing Page</p>
+    <div className="min-h-screen">
+      <p className="text-blue-700">Welcome to the Landing Page</p>
     </div>
   );
 }
